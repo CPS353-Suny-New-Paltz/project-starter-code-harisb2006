9fe8a1e120e892cc3fa2bb3406a72dc4206165ba
Haris Basic <basich2@newpaltz.edu>
2025-08-27 21:38:01 -0400

    test magic number check #2

--- src/project/annotations/ConceptualAPI.java
+++ src/project/annotations/ConceptualAPI.java
@@ -8,6 +8,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ConceptualAPI {
-    int BadNumber = 10;
+    int BAD_NUMBER = 10;
     // Marker annotation, should be applied to an interface type
 }
